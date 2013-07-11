2b3adc816adecf6c3754d7fccd4dfe90c8589964
David McAfee <davidmcafeegraphite@gmail.com>
2013-07-11 15:37:25 +0930

    Bug Fixes
    App no longer crashes

---
+++ DifferentialCalculus.m
@@ -0,0 +1,25 @@
+//
+//  Differential Calculus.m
+//  Formulae
+//
+//  Created by David McAfee on 7/07/13.
+//  Copyright (c) 2013 David McAfee. All rights reserved.
+//
+
+#import "DifferentialCalculus.h"
+
+@implementation DifferentialCalculus
+
+
+- (void)viewDidLoad
+{
+    [scroll setScrollEnabled:YES];
+    [scroll setContentSize:CGSizeMake(320, 1270)];
+    [super viewDidLoad];
+    
+    
+    
+}
+
+
+@end
